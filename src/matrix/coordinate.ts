2a6799d19ed3986087e5412351e4baa2ac2ee53b
Petr Rusanov <peter.rusanov@gmail.com>
2020-12-01 07:47:52 +0100

    Add MatrixCoordinate and Matrix classes

--- src/matrix/coordinate.ts
+++ src/matrix/coordinate.ts
@@ -1,7 +1,17 @@
+/**
+ * Holds (row, column) coordinate within the matrix
+ */
 export default class MatrixCoordinate {
+  // Matrix row
   readonly row: number;
+  // Matrix column
   readonly column: number;
 
+  /**
+   * Creates a matrix coordinate
+   * @param {number} row Matrix row
+   * @param {number} column Matrix column
+   */
   constructor(row: number, column: number) {
     this.row = row;
     this.column = column;
--- src/matrix/index.ts
+++ src/matrix/index.ts
@@ -1,10 +1,19 @@
 import MatrixCoordinate from './coordinate';
 
+/**
+ * Contains data and dimensional information of the matrix.
+ * Supports get/set operations as well as retrieving adjacent elements.
+ */
 export default class Matrix<Element> {
   private data: Element[][];
+
   readonly rows: number;
   readonly columns: number;
 
+  /**
+   * Creates a matrix holding provided data.
+   * @param data Two-dimensional array containing matrix elements.
+   */
   constructor(data: Element[][]) {
     if (data.length === 0) {
       throw new Error('matrix data is empty');
@@ -15,22 +24,49 @@ export default class Matrix<Element> {
     this.data = data;
   }
 
+  /**
+   * Gets element value using specified coordinate.
+   * @param {MatrixCoordinate} coordinate Element coordinate in the matrix.
+   * @returns {Element} Value at specified coordinate
+   */
   getAtCoordinate(coordinate: MatrixCoordinate): Element {
     return this.data[coordinate.row][coordinate.column];
   }
 
+  /**
+   * Gets element value at specified row and column.
+   * @param {number} row Matrix row
+   * @param {number} column Matrix column
+   * @returns {Element} Value at specified row and column
+   */
   get(row: number, column: number): Element {
     return this.data[row][column];
   }
 
+  /**
+   * Updates value at specified coordinate.
+   * @param {MatrixCoordinate} coordinate Coordinate within the matrix
+   * @param {Element} value New value to set
+   */
   setAtCoordinate(coordinate: MatrixCoordinate, value: Element) {
     this.data[coordinate.row][coordinate.column] = value;
   }
 
+  /**
+   * Updates value at specified row and column.
+   * @param {number} row Matrix row
+   * @param {number} column Matrix column
+   * @param {Element} value New value to set
+   */
   set(row: number, column: number, value: Element) {
     return (this.data[row][column] = value);
   }
 
+  /**
+   * Gets coordinate of the left adjacent of the specified coordinate.
+   * @param {MatrixCoordinate} coordinate Element coordinate in the matrix.
+   * @returns {MatrixCoordinate | null} Coordinate of the left adjacent or null if there is none.
+   */
   left(coordinate: MatrixCoordinate): MatrixCoordinate | null {
     if (coordinate.column <= 0) {
       return null;
@@ -39,6 +75,11 @@ export default class Matrix<Element> {
     return new MatrixCoordinate(coordinate.row, coordinate.column - 1);
   }
 
+  /**
+   * Gets coordinate of the right adjacent of the specified coordinate.
+   * @param {MatrixCoordinate} coordinate Element coordinate in the matrix.
+   * @returns {MatrixCoordinate | null} Coordinate of the right adjacent or null if there is none.
+   */
   right(coordinate: MatrixCoordinate): MatrixCoordinate | null {
     if (coordinate.column >= this.columns) {
       return null;
@@ -47,6 +88,11 @@ export default class Matrix<Element> {
     return new MatrixCoordinate(coordinate.row, coordinate.column + 1);
   }
 
+  /**
+   * Gets coordinate of the top adjacent of the specified coordinate.
+   * @param {MatrixCoordinate} coordinate Element coordinate in the matrix.
+   * @returns {MatrixCoordinate | null} Coordinate of the top adjacent or null if there is none.
+   */
   top(coordinate: MatrixCoordinate): MatrixCoordinate | null {
     if (coordinate.row <= 0) {
       return null;
@@ -55,6 +101,11 @@ export default class Matrix<Element> {
     return new MatrixCoordinate(coordinate.row - 1, coordinate.column);
   }
 
+  /**
+   * Gets coordinate of the bottom adjacent of the specified coordinate.
+   * @param {MatrixCoordinate} coordinate Element coordinate in the matrix.
+   * @returns {MatrixCoordinate | null} Coordinate of the bottom adjacent or null if there is none.
+   */
   bottom(coordinate: MatrixCoordinate): MatrixCoordinate | null {
     if (coordinate.row >= this.rows) {
       return null;
@@ -63,6 +114,14 @@ export default class Matrix<Element> {
     return new MatrixCoordinate(coordinate.row + 1, coordinate.column);
   }
 
+  /**
+   * Converts matrix to string with columns separated by space and
+   * rows separated by new line, e.g.:
+   * 1 0 1 0
+   * 0 1 0 0
+   * 0 0 1 0
+   * @returns {string} Matrix string representation
+   */
   toString(): string {
     return this.data.map((row) => row.join(' ')).join('\n');
   }
