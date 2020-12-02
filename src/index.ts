ef6f513271868438a89a8f3191a8112f53b8e582
Petr Rusanov <peter.rusanov@gmail.com>
2020-12-02 09:17:50 +0100

    Update index.ts with binary matrix reader from stdin

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,35 @@
 import { Binary } from './types';
 import Matrix from './matrix';
 import findDistances from './findDistances';
+import BinaryMatrixReader, { MatrixReaderEvent, MatrixReaderLimits } from './binaryMatrixReader';
 
-const data: Binary[][] = [
-  [0, 0, 0, 1],
-  [0, 0, 1, 1],
-  [0, 1, 1, 0],
-];
+async function readMatrices(input: NodeJS.ReadStream, limits?: MatrixReaderLimits): Promise<Matrix<Binary>[]> {
+  return new Promise((resolve, reject) => {
+    const matrixReader = new BinaryMatrixReader(input, limits);
+    matrixReader.on(MatrixReaderEvent.Error, (error: Error) => reject(error));
+    matrixReader.on(MatrixReaderEvent.Done, (matrices: Matrix<Binary>[]) => resolve(matrices));
+    matrixReader.start();
+  });
+}
 
-const matrix = new Matrix<Binary>(data);
+async function main() {
+  const limits: MatrixReaderLimits = {
+    count: 1000,
+    dimensions: {
+      rows: 182,
+      columns: 182,
+    },
+  };
 
-const distances = findDistances(matrix);
+  try {
+    const matrices = await readMatrices(process.stdin, limits);
+    const output = matrices.map((m) => findDistances(m)).join('\n\n');
+    console.log(output);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-console.log(matrix.toString());
-console.log(distances.toString());
+(async () => {
+  await main();
+})();
